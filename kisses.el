e9055f195dbe86823ad5bd2444caec6c84e95e34
glechismi <94998063+glechismi@users.noreply.github.com>
2022-03-23 01:38:49 -0400

    Update kisses.el

--- kisses.el
+++ kisses.el
@@ -7,19 +7,19 @@
 ;; Created: 2021-11-26
 ;; Modified: 2021-11-26
 ;; Version: 0.0.1
-;; Keywords: 
+;; Keywords:
 ;; Homepage: https://github.com/jsilve24/kisses
 ;; Package-Requires: ((emacs "24.3") (dash "2.19.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
-;; 
+;;
 ;; This is a simple library that creates a splash buffer with `kisses-banner'
-;; centered vertically and horizontally. See git readme for more details. 
+;; centered vertically and horizontally. See git readme for more details.
 ;;
 ;;; Code:
-
+;; (require 'splash-screen)
 (require 'dash)
 
 ;; Calculate dimensions of largest monitor
@@ -32,16 +32,16 @@
 
 
 (defvar kisses-banner
-  "@@@@@@@@  @@@@@@@@@@    @@@@@@    @@@@@@@   @@@@@@   
-@@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@   
-@@!       @@! @@! @@!  @@!  @@@  !@@       !@@       
-!@!       !@! !@! !@!  !@!  @!@  !@!       !@!       
-@!!!:!    @!! !!@ @!@  @!@!@!@!  !@!       !!@@!!    
-!!!!!:    !@!   ! !@!  !!!@!!!!  !!!        !!@!!!   
-!!:       !!:     !!:  !!:  !!!  :!!            !:!  
-:!:       :!:     :!:  :!:  !:!  :!:           !:!   
-:: ::::   :::     ::   ::   :::   ::: :::  :::: ::   
-: :: ::    :      :     :   : :   :: :: :  :: : :    
+  "@@@@@@@@  @@@@@@@@@@    @@@@@@    @@@@@@@   @@@@@@
+@@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@
+@@!       @@! @@! @@!  @@!  @@@  !@@       !@@
+!@!       !@! !@! !@!  !@!  @!@  !@!       !@!
+@!!!:!    @!! !!@ @!@  @!@!@!@!  !@!       !!@@!!
+!!!!!:    !@!   ! !@!  !!!@!!!!  !!!        !!@!!!
+!!:       !!:     !!:  !!:  !!!  :!!            !:!
+:!:       :!:     :!:  :!:  !:!  :!:           !:!
+:: ::::   :::     ::   ::   :::   ::: :::  :::: ::
+: :: ::    :      :     :   : :   :: :: :  :: : :
 "
   "Banner to display on startup.")
 
@@ -53,9 +53,9 @@
   "Returns list (row col) giving dimensions of bounding box of
   kisses-banner."
   (let* ((strings (split-string kisses-banner "\n"))
-	 (string-lengths (-map 'length strings))
-	 (ncol (apply 'max string-lengths))
-	 (nrow (length strings)))
+         (string-lengths (-map 'length strings))
+         (ncol (apply 'max string-lengths))
+         (nrow (length strings)))
     (setq kisses--box-dimensions (list nrow ncol))))
 
 (defvar kisses--insertion-point nil
@@ -72,12 +72,12 @@
   (setq left-fringe-width 0)
   (setq right-fringe-width 0)
   (set-display-table-slot standard-display-table 'truncation 32)
-  (set-window-buffer (selected-window) (get-buffer "*splash*"))
+  (set-window-buffer (selected-window) (get-buffer "*kisses*"))
   (setq cursor-type nil)
   (face-remap-add-relative 'region '(:inherit default))
   (if (fboundp 'evil-mode)
       (setq-local evil-normal-state-cursor nil)
-      (setq-local evil-emacs-state-cursor nil)
+    (setq-local evil-emacs-state-cursor nil)
     (setq-local cursor-type nil)))
 
 (define-derived-mode kisses-mode
@@ -86,29 +86,29 @@
   ;; bit of setup to make display nice
   (kisses--set-local-vars))
 
-;; make buffer of size equal to largest monitor store center of text coordinates 
+;; make buffer of size equal to largest monitor store center of text coordinates
 (defun kisses--make-splash-buffer ()
   "Creates buffer of dimension `kisses--max-columns' and `kisses--max-rows' and places the
-banner at the center. Also checks to see if buffer named *splash* already exists and if so overwrites it"
+banner at the center. Also checks to see if buffer named *kisses* already exists and if so overwrites it"
   (unless kisses--box-dimensions
     (kisses--banner-box-dimenions))
-  (let* ((splash-buffer (get-buffer-create "*splash*"))
-	 (height (/ kisses--max-rows 2))
-	 (width (/ kisses--max-columns 2))
-	 (box-top (/ (car kisses--box-dimensions) 2))
-	 (box-left (/ (nth 1 kisses--box-dimensions) 2))
-	 (padding-top (- height box-top))
-	 (padding-left (- width box-left))
-	 (top-pad-string (concat (make-string kisses--max-columns ?\s) "\n")))
+  (let* ((splash-buffer (get-buffer-create "*kisses*"))
+         (height (/ kisses--max-rows 2))
+         (width (/ kisses--max-columns 2))
+         (box-top (/ (car kisses--box-dimensions) 2))
+         (box-left (/ (nth 1 kisses--box-dimensions) 2))
+         (padding-top (- height box-top))
+         (padding-left (- width box-left))
+         (top-pad-string (concat (make-string kisses--max-columns ?\s) "\n")))
     (switch-to-buffer splash-buffer)
     (read-only-mode -1)
     (kisses--set-local-vars)
     (if (string= major-mode "kisses-mode")
-	(erase-buffer)
+        (erase-buffer)
       nil)
     (dotimes (_ padding-top) (insert top-pad-string))
     (let ((tmp-point (point))
-	  (indent-tabs-mode nil))
+          (indent-tabs-mode nil))
       (insert kisses-banner)
       (mark-paragraph)
       (indent-region (point) (mark) padding-left)
@@ -119,17 +119,17 @@ banner at the center. Also checks to see if buffer named *splash* already exists
       (deactivate-mark)
       (read-only-mode 1)
       (kisses-mode)
-      (get-buffer "*splash*"))))
+      (get-buffer "*kisses*"))))
 
 
 (defun kisses--set-window-start (window)
   "Set window start to center banner in `window'."
   ;; look at set-window-start function
   (let* ((height (window-body-height nil))
-	 (width (window-total-width nil))
-	 (box-top (/ (nth 0 kisses--box-dimensions) 2))
-	 (box-left (/ (nth 1 kisses--box-dimensions) 2))
-	 (calling-window (selected-window)))
+         (width (window-total-width nil))
+         (box-top (/ (nth 0 kisses--box-dimensions) 2))
+         (box-left (/ (nth 1 kisses--box-dimensions) 2))
+         (calling-window (selected-window)))
     (select-window window)
     (kisses--set-local-vars)
     ;; now acctually set window start
@@ -154,17 +154,19 @@ banner at the center. Also checks to see if buffer named *splash* already exists
   (kisses--set-window-start (selected-window)))
 
 (defun kisses-initial-buffer ()
+  (interactive)
   "Function designed to be called by initial buffer."
+  ;; (kill-buffer "*splash*")
   (kisses-redraw)
-  (get-buffer "*splash*"))
+  (get-buffer "*kisses*"))
 
 (defun kisses-window-size-change-function (arg)
   "Funtion to run on window size change."
-  ;; get list of windows displaying "*splash*"
-  (when (get-buffer "*splash*")
+  ;; get list of windows displaying "*kisses*"
+  (when (get-buffer "*kisses*")
     ;; (if (string= major-mode "kisses-mode")
-    ;; 	(kisses--set-window-start (selected-window)))
-    (let ((w-to-update (get-buffer-window-list "*splash*" nil (selected-frame))))
+    ;;  (kisses--set-window-start (selected-window)))
+    (let ((w-to-update (get-buffer-window-list "*kisses*" nil (selected-frame))))
       (-map 'kisses--set-window-start w-to-update))
     ))
 
@@ -173,6 +175,18 @@ banner at the center. Also checks to see if buffer named *splash* already exists
 
 ;; (add-hook 'window-size-change-functions (lambda (arg) (message "size change detected")))
 ;; (add-hook 'window-startup-hook 'kisses-window-size-change-function)
+
+(if (and (not (member "-no-splash"  command-line-args))
+         (not (member "--file"      command-line-args))
+         (not (member "--insert"    command-line-args))
+         (not (member "--find-file" command-line-args))
+         (not inhibit-startup-screen)
+         )
+    (progn
+      (add-hook 'window-setup-hook 'kisses-initial-buffer)
+      (setq inhibit-startup-screen t
+            inhibit-startup-message t
+            inhibit-startup-echo-area-message t)))
 
 (provide 'kisses)
 
